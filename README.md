b0493f423883582cc7b92a517d0892bc5e13cec7
Janrey Licas <janjan.licas@gmail.com>
2020-10-19 18:06:33 +0800

    Added Initialized Files on Other Modules
    This was not the initial work. Please stay tuned.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Discord-Activity-README-Badge
+# Discord Activity README Badge
+
 A Discord-Based Activity Identifier by a Bot for your Activity Presence Badge on your README. Made on Python.
+
+
+This is currently under work. Initial work will be committed later.
---
+++ experiments/activity.py
@@ -0,0 +1,21 @@
+import discord
+README_LOCATION = "README.md"
+
+EXIT_FAILURE = 1
+EXIT_SUCCESS = 0
+
+class MyClient(discord.Client):
+    #def __init__(self, **kwargs):
+    #    pass
+        #if not kwargs:
+        #    print("")
+        #    raise SystemExit("Error: Cannot Run Process with Few Arguments Missing.")
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('NzY3NjAyMzAyMTIyMTk2OTky.X40TWg.MfsCEjMzaeYO7etqaqXlZ2YkqQI')
